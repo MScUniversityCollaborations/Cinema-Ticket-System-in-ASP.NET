fc879f37a67f87939360ccaaad2d9a87a5e5dc71
Zartilas <zartilasvasilios@hotmail.gr>
2022-02-11 04:19:38 +0200

    Bug Fixed

---
+++ MovieTickets/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult NowShowing()
+        {
+            return View();
+        }
+
+        public ActionResult ComingSoon()
+        {
+            return View();
+        }
+    }
+}

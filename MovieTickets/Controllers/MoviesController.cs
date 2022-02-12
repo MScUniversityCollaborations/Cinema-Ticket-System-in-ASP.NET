f4c0126359f69301da9e72c52af752dcee1301d6
Zartilas <zartilasvasilios@hotmail.gr>
2022-02-12 12:14:30 +0200

    Bug Fixed

--- MovieTickets/Controllers/MoviesController.cs
+++ MovieTickets/Controllers/MoviesController.cs
@@ -22,5 +22,10 @@ namespace MovieTickets.Controllers
         { 
             return View();
         }
+
+        public ActionResult BuyTicket()
+        {
+            return View();
+        }
     }
 }

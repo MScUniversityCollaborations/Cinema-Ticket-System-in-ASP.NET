a27430771751e74a660ddd34d8bbabfeae0d3952
Νικόλας Π <nikolas.98@live.com>
2022-02-13 10:00:47 +0200

    Update the account, manage controller and fixed a few issues.

--- MovieTickets/Models/AccountViewModels.cs
+++ MovieTickets/Models/AccountViewModels.cs
@@ -65,6 +65,18 @@ namespace MovieTickets.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The {0} must contain only letters!")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The {0} must contain only letters!")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }

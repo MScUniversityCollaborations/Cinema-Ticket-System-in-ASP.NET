58357d798e3d76fe368f01fff91e555afe450f32
Zartilas <zartilasvasilios@hotmail.gr>
2022-02-15 16:22:16 +0200

    Imported Calendar

---
+++ MovieTickets/Controllers/Api/ScreeningsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieTickets.Controllers.Api
+{
+    public class ScreeningsController : Controller
+    {
+        // GET: Screenings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetScreenings()
+        {
+            /*using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }*/
+
+            return new JsonResult { Data = "{}", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
